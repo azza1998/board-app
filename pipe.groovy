dec5eaea4668e35e40eeb77a032cd7a05503bfaf
azza1998 <62287515+azza1998@users.noreply.github.com>
2022-05-17 11:08:04 +0100

    Update pipe.groovy

--- pipe.groovy
+++ pipe.groovy
@@ -1,4 +1,3 @@
-def version, mvnCmd = "mvn -s nexus.xml"
 pipeline{
   agent{
     label "master"
@@ -8,16 +7,34 @@ pipeline{
               steps {
              
                 git url: "https://github.com/azza1998/Notes-apps.git", branch: "main"
-                script {
-                  def pom = readMavenPom file: 'pom.xml'
-                  version = pom.version
-              }
               }
             }
-            stage("Archive App") {
-            steps {
-              sh "${mvnCmd} deploy -DskipTests=true -P nexus3"
-            }
-          }
- }
+            stage ("Sonarqube Analysis"){
+                steps{
+                    def sonarHome =  tool 'SQ';
+                    sonarIP = getIP('sonarqube.sonarqube.svc.cluster.local.')
+                    sh "${sonarHome}/bin/sonar-scanner -Dsonar.projectKey=Notes-app -Dsonar.projectName=Notes-app -Dsonar.host.url=http://${sonarIP}:9000 -Dsonar.login=admin -Dsonar.password=azza"
+                    }}
+            stage("Create App"){
+                steps{
+                
+                    sh "oc apply -f openshift-specifications/ -n notes-app"
+                    sh "oc apply -f openshift-specifications/with-dockerfile -n notes-app"
+                    }
+                }
+            stage ("Build & Push image to Nexus"){
+                steps{
+                openshiftBuild(bldCfg: 'notes-app', showBuildLogs: 'true', namespace: 'notes-app')
+
+            }}
+            stage ("Deploy App"){
+                steps{
+                openshiftVerifyDeployment(namespace: 'notes-app', depCfg: 'notes-app', replicaCount:'1', verifyReplicaCount: 'true', waitTime: '180000')
+
+            }}
+            
+    
+    
+}
+    
 }
